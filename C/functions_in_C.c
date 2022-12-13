fdee05668c0933e9dc2a1ccc8d8862bcfb086e8d
AmirAlakbarli <emirelekberli@gmail.com>
2022-12-13 04:03:02 +0400

    5 tasks related to C language have been solved

---
+++ C/functions_in_C.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int max_of_four(int a, int b, int c, int d)
+{
+    return a > b ? (c > d ? (a > c ? a : c) : (a > d ? a : d)) : (c > d ? (b > c ? b : c) : (b > d ? b : d));
+}
+
+int main()
+{
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    return 0;
+}
